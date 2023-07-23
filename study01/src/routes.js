2efa3cb8e7bb79466b8838660cb532f28575dc03
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-23 14:11:22 -0300

    use Router

---
+++ study01/src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from './router.js'
+import { Database } from './database.js'
+
+const database = new Database()
+
+await database.migrate('createTable', {name: 'users'})
+
+export const router = new Router()
+
+router.add('/users', 'GET', (req, res) => {
+    return res.end(JSON.stringify(database.select('users')))
+})
+
+router.add('/users', 'POST', (req, res) => {
+    return res.writeHead(201).end(JSON.stringify(database.insert('users', req.body)))
+})
--- study01/src/server.js
+++ study01/src/server.js
@@ -1,25 +1,14 @@
 import http from 'node:http'
-import {Database} from './database.js'
 import {useJSONMiddleware} from './middlewares/json.js'
-
-
-const database = new Database()
-
-await database.migrate('createTable', {name: 'users'})
+import { router } from './routes.js'
 
 
 const server = http.createServer(async (req, res) => {
     await useJSONMiddleware(req, res)
 
-    if (req.method === 'GET' && req.url === '/users') {
-        return res.end(JSON.stringify(database.select('users')))
-    }
+    const handler = router.getHandler(req.url, req.method)
 
-    if (req.method === 'POST' && req.url === '/users') {       
-        return res.writeHead(201).end(JSON.stringify(database.insert('users', req.body)))
-    }
-    
-    return res.writeHead(404).end()
+    return handler ? handler(req, res) : res.writeHead(404).end()
 })
 
 server.listen(3333)
